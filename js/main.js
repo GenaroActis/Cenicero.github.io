503f0418e0b5dc9ab442bfe69536ca082f9d5f73
GenaroActis <113357267+GenaroActis@users.noreply.github.com>
2023-01-12 19:09:15 -0300

    ult

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-const stockProductos = [
-    { id:1, nombre:"Chomba Champion", precio:5000, img1:"../img/2.png", img2:"../img/1.png", stock: 2, talle:"M", precioSubTotal:5000},
-    { id:2, nombre:"Buzo Michigan", precio:7500, img1:"../img/4.png", img2:"../img/5.png", stock: 2, talle:"XL", precioSubTotal:7500},
-    { id:3, nombre:"Buzo Raiders", precio:8000, img1:"../img/6.png", img2:"../img/7.png", stock: 2, talle:"XL", precioSubTotal:8000},
-    { id:4, nombre:"Chomba Polo Ralph Lauren", precio:4000, img1:"../img/9.png", img2:"../img/10.png", stock: 2, talle:"L", precioSubTotal:4000},
-    { id:5, nombre:"Chomba Tommy Hilfiger", precio:3500, img1:"../img/11.png", img2:"../img/12.png", stock: 2, talle:"L", precioSubTotal:3500},
-    { id:6, nombre:"Chomba Tommy Hilfiger", precio:2500, img1:"../img/13.png", img2:"../img/14.png", stock: 2, talle:"M", precioSubTotal:2500},
-    { id:7, nombre:"Chomba Polo Ralph Lauren", precio:4500, img1:"../img/15.png", img2:"../img/16.png", stock: 2, talle:"L", precioSubTotal:4500},
-    { id:8, nombre:"Chomba RLX Ralph Lauren", precio:4500, img1:"../img/17.png", img2:"../img/18.png", stock: 2, talle:"M", precioSubTotal:4500},
-] 
+// const stockProductos = [
+//     { id:1, nombre:"Chomba Champion", precio:5000, img1:"../img/2.png", img2:"../img/1.png", stock: 2, talle:"M", precioSubTotal:5000},
+//     { id:2, nombre:"Buzo Michigan", precio:7500, img1:"../img/4.png", img2:"../img/5.png", stock: 2, talle:"XL", precioSubTotal:7500},
+//     { id:3, nombre:"Buzo Raiders", precio:8000, img1:"../img/6.png", img2:"../img/7.png", stock: 2, talle:"XL", precioSubTotal:8000},
+//     { id:4, nombre:"Chomba Polo Ralph Lauren", precio:4000, img1:"../img/9.png", img2:"../img/10.png", stock: 2, talle:"L", precioSubTotal:4000},
+//     { id:5, nombre:"Chomba Tommy Hilfiger", precio:3500, img1:"../img/11.png", img2:"../img/12.png", stock: 2, talle:"L", precioSubTotal:3500},
+//     { id:6, nombre:"Chomba Tommy Hilfiger", precio:2500, img1:"../img/13.png", img2:"../img/14.png", stock: 2, talle:"M", precioSubTotal:2500},
+//     { id:7, nombre:"Chomba Polo Ralph Lauren", precio:4500, img1:"../img/15.png", img2:"../img/16.png", stock: 2, talle:"L", precioSubTotal:4500},
+//     { id:8, nombre:"Chomba RLX Ralph Lauren", precio:4500, img1:"../img/17.png", img2:"../img/18.png", stock: 2, talle:"M", precioSubTotal:4500},
+// ] 
 
-const producto = document.querySelector("#productos");
+const productosApp = document.querySelector("#productos");
 const modalBody = document.querySelector("#modal-body");
 const fotterModalPadre = document.querySelector("#modalFooter");
+const botonProcesarCompra = document.querySelector("#botonProcesarCompra");
+const procesarCompra = document.querySelector("#procesarCompra");
+const productosJson = "../json/productos.json";
 
 let productosElegidos = [];
 
+function compra() {
+    productosElegidos.forEach((producto) => {
+        const divCompra = document.createElement('div');
+        divCompra.classList.add('divCompra');
+        divCompra.innerHTML += `
+        <div class="modal-contenedor">
+            <div>
+                <img class="img-fluid img-carrito" src="${img}"/>
+            </div>
+                <div>
+                    <p>Producto: ${nombre}</p>
+                    <p>Precio: ${precio}</p>
+                    <p>Cantidad :${cantidad}</p>
+                    <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+                </div>
+            </div>
+        `
+        procesarCompra.appendChild(divCompra);
+    });
+    };
 
 function guardarLocal() {
     localStorage.setItem("productosElegidos", JSON.stringify(productosElegidos));
@@ -22,9 +45,27 @@ function guardarLocal() {
 if(localStorage.getItem("productosElegidos")){
     productosElegidos = JSON.parse(localStorage.getItem("productosElegidos"));
 }
+    botonProcesarCompra.addEventListener("click", () => {
+        if (productosElegidos.length === 0) {
+            console.log("carrito vacio")
+        // Swal.fire({
+        //     title: "¡Tu carrito está vacio!",
+        //     icon: "error",
+        //     confirmButtonText: "Aceptar",
+        // });
+        } else {
+        location.href = "compra.html";
+        compra();
+        }
+    });
 
-// cards productos
-stockProductos.forEach ((producto) =>{
+
+// traemos los productos del json
+fetch(productosJson)
+.then(respuesta => respuesta.json())
+.then(productos => {
+    // generamos las cards con productos
+    productos.forEach( producto => {
     const divCard = document.createElement('div');
     const img1 = document.createElement('img');
     const img2 = document.createElement('img');
@@ -55,6 +96,13 @@ stockProductos.forEach ((producto) =>{
     // funcion agregar producto
     atag.addEventListener("click", (e) => {
         agregarProducto();
+        Toastify({
+            text: "Agregado al Carrito",
+            duration: 3000,
+            style: {
+                background: "#da8a0d",
+            }
+            }).showToast();
         total();
         mostrarCarrito();
     });
@@ -90,10 +138,11 @@ const agregarProducto = (id) =>{
                 console.log("click")
                 console.log(productoEnCarrito);
     };
+    
     guardarLocal();
 };  
     
-    productos.append(divCard);
+    productosApp .append(divCard);
     divCard.append(img1,divCardBody);
     divCard.append(img2,divCardBody);
     divCardBody.append(h5tag,ptagTalle,ptagPrecio,atag);
@@ -108,21 +157,21 @@ const agregarProducto = (id) =>{
     });
 
     // funcion selector de img 
-    img2.addEventListener('click', function(){
-        // img1.src = this.src
-        // img2.src = producto.img1
-
-        if (img1.src = producto.img1){
-        img1.src = producto.img2
-        img2.src = producto.img1
-        }
-        else if (img1.src = producto.img2){
-            img1.src = producto.img1
-            img2.src = producto.img2
-        };
-    });
     
+img1.addEventListener("mouseover", () => {
+    img1.src = producto.img2;
+    img2.src = producto.img1;
 });
+img1.addEventListener("mouseout", () => {
+    img1.src = producto.img1;
+    img2.src = producto.img2;
+});
+})
+})
+.catch(error => console.log(error))
+
+
+
 
 
 // boton Carrito
@@ -197,7 +246,6 @@ const mostrarCarrito = () => {
                     let productoParaActualizar = productosElegidos.find((prod) =>{
                         return prod.id == producto.id
                     });
-                    
                     productoParaActualizar.elegidos = productoParaActualizar.elegidos - 1;
                     mostrarCarrito();
                     total();
@@ -236,3 +284,5 @@ botonVaciarCarrito.addEventListener("click", (e) => {
 fotterModalPadre.append(botonVaciarCarrito);
 fotterModalPadre.append(fotterModal);
 };
+
+
